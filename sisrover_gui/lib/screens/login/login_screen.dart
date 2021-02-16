df64ef2e27ad5b55620dff427205c235eb8794ce
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2021-02-16 01:25:53 -0500

    changed title window to "Smart Imaging Systems, Inc.

--- sisrover_gui/lib/screens/login/login_screen.dart
+++ sisrover_gui/lib/screens/login/login_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:window_size/window_size.dart';
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    setWindowTitle("Smart Imaging Systems, Inc.");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
