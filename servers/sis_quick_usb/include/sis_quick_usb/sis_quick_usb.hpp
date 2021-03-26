dbe3e3e09a5cf6a9a41b7298741a9166008c7212
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2021-03-26 19:14:01 -0400

    fixed issue with quickusb compiling on updated gcc

--- servers/sis_quick_usb/include/sis_quick_usb/sis_quick_usb.hpp
+++ servers/sis_quick_usb/include/sis_quick_usb/sis_quick_usb.hpp
@@ -1,6 +1,19 @@
 #ifndef SIS_QUICK_USB_HPP
 #define SIS_QUICK_USB_HPP
 
-namespace squsb {}  // namespace squsb
+namespace squsb {
+class squsb {
+public:
+    squsb();
+
+    bool connect_to_qusb(const char begin_serial_number, signed long timeout);
+
+private:
+    bool connected = false;
+
+    char current_begin_serial_number;
+    signed long current_quick_usb_timeout;
+};
+}  // namespace squsb
 
 #endif
--- servers/sis_quick_usb/sis_quick_usb.cpp
+++ servers/sis_quick_usb/sis_quick_usb.cpp
@@ -1,5 +1,16 @@
 #include "sis_quick_usb.hpp"
 
 #include <iostream>
+#include <vector>
 
-void say_hello() { std::cout << "Hello, from sis_quick_usb!\n"; }
+squsb::squsb::squsb() {}
+
+bool squsb::squsb::connect_to_qusb(const char begin_serial_number, signed long timeout) {
+    std::cout << "squsb: attempting to connect to quickusb" << std::endl;
+    current_begin_serial_number = begin_serial_number;
+    current_quick_usb_timeout = timeout;
+
+    char dev_name[100];
+    std::vector<int> dev_idx;
+
+}
