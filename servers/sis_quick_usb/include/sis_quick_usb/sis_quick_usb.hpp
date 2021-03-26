eccdc2df4105df468c4a735b5f1c91812e5fde0b
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2021-03-26 18:21:55 -0400

    creating shell for sis quick usb class

---
+++ servers/sis_quick_usb/include/sis_quick_usb/sis_quick_usb.hpp
@@ -0,0 +1,6 @@
+#ifndef SIS_QUICK_USB_HPP
+#define SIS_QUICK_USB_HPP
+
+namespace squsb {}  // namespace squsb
+
+#endif
--- servers/sis_quick_usb/sis_quick_usb.cpp
+++ servers/sis_quick_usb/sis_quick_usb.cpp
@@ -1,5 +1,5 @@
+#include "sis_quick_usb.hpp"
+
 #include <iostream>
 
-void say_hello(){
-    std::cout << "Hello, from sis_quick_usb!\n";
-}
+void say_hello() { std::cout << "Hello, from sis_quick_usb!\n"; }
