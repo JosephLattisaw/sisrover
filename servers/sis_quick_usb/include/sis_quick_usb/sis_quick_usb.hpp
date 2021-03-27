5b52cb8aac269e02b3e9f892d12b6df92a2a447e
Joseph Lattisaw <joseph.lattisaw@nasa.gov>
2021-03-26 21:08:02 -0400

    finished creating sis_quick_usb library

--- servers/sis_quick_usb/include/sis_quick_usb/sis_quick_usb.hpp
+++ servers/sis_quick_usb/include/sis_quick_usb/sis_quick_usb.hpp
@@ -1,6 +1,9 @@
 #ifndef SIS_QUICK_USB_HPP
 #define SIS_QUICK_USB_HPP
 
+#include <tuple>
+#include <vector>
+
 #include "QuickUSB.h"
 
 namespace squsb {
@@ -12,13 +15,37 @@ public:
     bool connect_to_qusb(const char begin_serial_number, QLONG timeout);
     bool disconnect_from_qusb();
 
+    // fpga
+    std::tuple<bool, unsigned char> read_fpga(QWORD address);
+    bool write_fpga(QWORD address, char value);
+
+    // quickusb commands
+    bool read_quickusb_command(QWORD address, unsigned char* destination, QWORD* length);
+    bool write_quickusb_command(QWORD address, unsigned char* data, QWORD length);
+
+    // other quickusb commands
+    std::tuple<bool, unsigned char> read_adc(QWORD address, unsigned char* data);
+    bool read_data(unsigned char* data, unsigned long* length);
+    bool set_DAC(unsigned char dac, unsigned char channel, unsigned char value);
+    bool set_port_direction(int port, int direction);
+    bool write_port(unsigned short address, unsigned char* data, unsigned short length);
+
+    // other
+    std::string get_version_number() const { return VERSION_NUMBER; }
+
 private:
+    // print helpers
+    void print_dev_null_error();
     void print_last_error_message();
+
+    unsigned char command_and_chan_val(unsigned char cmd, unsigned chan);
 
     char current_begin_serial_number;
     QLONG current_quick_usb_timeout;
 
     QHANDLE dev_handle = nullptr;
+
+    const std::string VERSION_NUMBER = "1.2.1";
 };
 }  // namespace squsb
 
--- servers/sis_quick_usb/sis_quick_usb.cpp
+++ servers/sis_quick_usb/sis_quick_usb.cpp
@@ -4,6 +4,12 @@
 #include <iostream>
 #include <vector>
 
+namespace {
+const unsigned char POWERDAC = 0x03;
+const unsigned char DAC_ADDRESS[3] = {0x10, 0x11, 0x12};
+const unsigned char ADC_ADDRESS[3] = {0x48, 0x49, 0x4a};
+}  // namespace
+
 squsb::squsb::squsb() {}
 
 bool squsb::squsb::connect_to_qusb(const char begin_serial_number, QLONG timeout) {
@@ -31,7 +37,7 @@ bool squsb::squsb::connect_to_qusb(const char begin_serial_number, QLONG timeout
         return false;
     }
 
-    for (const auto &i : dev_idx) {
+    for (const auto& i : dev_idx) {
         auto dn = std::string(&dev_name[i]);
         if (QuickUsbOpen(&dev_handle, &dev_name[i])) {
             std::cout << "squsb: opened quickusb device: " << dn << std::endl;
@@ -89,6 +95,120 @@ bool squsb::squsb::disconnect_from_qusb() {
     return true;
 }
 
+std::tuple<bool, unsigned char> squsb::squsb::read_fpga(QWORD address) {
+    unsigned char dat[1];
+    QWORD l = sizeof(dat);
+    bool res = read_quickusb_command(address, &dat[0], &l);
+    return std::make_tuple(res, dat[0]);
+}
+
+bool squsb::squsb::write_fpga(QWORD address, char value) {
+    unsigned char dat[1];
+    dat[1] = value;
+    QWORD l = sizeof(dat);
+    write_quickusb_command(address, &dat[0], l);
+    return write_quickusb_command(address, &dat[0], l);  // writing a second time just in case
+}
+
+bool squsb::squsb::read_quickusb_command(QWORD address, unsigned char* destination, QWORD* length) {
+    std::cout << "squsb: reading quickusb command at address: " << static_cast<int>(address)
+              << std::endl;
+    if (!QuickUsbReadCommand(dev_handle, address, destination, length)) {
+        std::cerr << "squsb: failed to read command from QuickUSB" << std::endl;
+        return false;
+    }
+
+    return true;
+}
+
+bool squsb::squsb::write_quickusb_command(QWORD address, unsigned char* data, QWORD length) {
+    std::cout << "squsb: writing quickusb command to address " << static_cast<int>(address)
+              << std::endl;
+
+    if (dev_handle == nullptr)
+        print_dev_null_error();
+    else if (!QuickUsbWriteCommand(dev_handle, address, data, length)) {
+        std::cerr << "squsb: failed to write quickusb command" << std::endl;
+        print_last_error_message();
+    } else
+        return true;
+
+    return false;
+}
+
 void squsb::squsb::print_last_error_message() {
-    // TODO
+    QULONG ec;
+    QuickUsbGetLastError(&ec);
+    std::cerr << "squsb: last error code: " << ec << std::endl;
+}
+
+void squsb::squsb::print_dev_null_error() {
+    std::cerr << "squsb: failed: device handle is null" << std::endl;
+}
+
+std::tuple<bool, unsigned char> squsb::squsb::read_adc(QWORD address, unsigned char* data) {
+    if (dev_handle == nullptr) {
+        print_dev_null_error();
+        return std::make_tuple(false, 0);
+    }
+
+    // set the channel for the ADC
+    auto res = QuickUsbWriteI2C(dev_handle, address, data, 1);
+
+    // read the value
+    unsigned short l = 1;
+    unsigned char val;
+    res = QuickUsbReadI2C(dev_handle, address, &val, &l);
+    return std::make_tuple(static_cast<bool>(res), val);
+}
+
+bool squsb::squsb::read_data(unsigned char* data, unsigned long* length) {
+    if (dev_handle != nullptr && QuickUsbReadData(dev_handle, data, length))
+        return true;
+    else if (dev_handle == nullptr)
+        print_dev_null_error();
+
+    std::cerr << "squsb: failed to read data from QuickUsb" << std::endl;
+    print_last_error_message();
+    return false;
+}
+
+bool squsb::squsb::set_DAC(unsigned char dac, unsigned char channel, unsigned char value) {
+    std::cout << "squsb: setting DAC " << static_cast<int>(dac) << " channel "
+              << static_cast<int>(channel) << " value " << static_cast<int>(value) << std::endl;
+
+    unsigned char dat[] = {command_and_chan_val(POWERDAC, channel), value, 0};
+}
+
+unsigned char squsb::squsb::command_and_chan_val(unsigned char cmd, unsigned chan) {
+    unsigned char ret = (cmd << 4) | (chan & 0x0f);
+    return ret;
+}
+
+bool squsb::squsb::set_port_direction(int port, int direction) {
+    std::cout << "squsb: setting port " << port << " to direction " << direction << std::endl;
+
+    if (dev_handle == nullptr)
+        print_dev_null_error();
+    else if (!QuickUsbWritePortDir(dev_handle, port, direction)) {
+        std::cerr << "squsb: failed to write quickusb port direction" << std::endl;
+        print_last_error_message();
+    } else
+        return true;
+
+    return false;
+}
+
+bool squsb::squsb::write_port(unsigned short address, unsigned char* data, unsigned short length) {
+    std::cout << "writing port " << static_cast<int>(address) << std::endl;
+
+    if (dev_handle == nullptr)
+        print_dev_null_error();
+    else if (!QuickUsbWritePort(dev_handle, address, data, length)) {
+        std::cerr << "squsb: failed to write port value" << std::endl;
+        print_last_error_message();
+    } else
+        return true;
+
+    return false;
 }
